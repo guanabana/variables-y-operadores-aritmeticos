195b66f20e866dbf1d0c63ab20bf8da8feaab9be
guanabana <jemarouy@gmail.com>
2023-10-12 11:07:17 -0300

    avances ejercicios 2 y 3

--- codigo.js
+++ codigo.js
@@ -3,6 +3,14 @@ function misEventos() {
   document
     .querySelector("#btnAgrupar")
     .addEventListener("click", leerApellidoYNombre);
+
+  document
+    .querySelector("#btnSumarej2")
+    .addEventListener("click", sumarDosValores);
+
+  document
+    .querySelector("#btnSumarej3")
+    .addEventListener("click", sumarTresValores);
 }
 
 //ejercicio 1
@@ -12,3 +20,27 @@ function leerApellidoYNombre() {
   document.querySelector("#txtMostrarConcatenado").innerHTML =
     apellido1 + "," + nombre1;
 }
+
+//ejercicio 2
+function sumarDosValores() {
+  let valor1txt = document.querySelector("#txtValor1ej2").value;
+  let valor2txt = document.querySelector("#txtValor2ej2").value;
+  let valor1Num = Number(valor1txt);
+  let valor2Num = Number(valor2txt);
+  let sumarValores = valor1Num + valor2Num;
+  document.querySelector("#txtMostrarSumaEj2").innerHTML = sumarValores;
+}
+
+//ejercicio 3
+function sumarTresValores() {
+  let valor1txt = document.querySelector("#txtValor1ej3").value;
+  let valor2txt = document.querySelector("#txtValor2ej3").value;
+  let valor3txt = document.querySelector("#txtValor3ej3").value;
+  let valor1Num = Number(valor1txt);
+  let valor2Num = Number(valor2txt);
+  let valor3Num = Number(valor3txt);
+  let sumarValores = valor1Num + valor2Num + valor3Num;
+  document.querySelector("#txtMostrarSumaEj3").innerHTML = sumarValores;
+}
+
+// ejercicio 4
