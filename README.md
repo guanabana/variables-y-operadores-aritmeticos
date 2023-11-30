d629b5ca0fa5ef933f8213d1fa513b039c24e18d
guanabana <jemarouy@gmail.com>
2023-11-30 13:18:35 -0300

    Hasta el Erjercicio 14

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # variables-y-operadores-aritmeticos
-práctico con el que fui aprendiendo sobre el uso de variables y operadores aritméticos, además de afianzar conceptos básicos de programación orientada a objetos. Utilizando html  y javascript en VSC.
+
+práctico con el que fui aprendiendo sobre el uso de variables y operadores aritméticos, además de afianzar conceptos básicos de programación orientada a objetos. Utilizando html y javascript en VSC.
+
+Primero armar el html, por cada ejercicio ir identificando las variables a usarse y su Id el cual debe ser único. Vincular el html con archivo javascript y empezar a practicar :)
+
+Ejercicio 1
+leer el nombre y apellido de una persona. Mostrar con el formato “apellido, nombre” primero el apellido, una coma, Luego el nombre. Tener en cuenta que se deben concatenar cadenas de texto y variables además se debe incluir un espacio en blanco luego de la coma.
+
+Ejercicio 2
+ingresar 2 valores y calcular la suma de ambos valores. Tener en cuenta que para operar aritméticamente con los 2 valores debemos asegurarnos que sean valores numéricos.
+
+Ejercicio 3
+ingresar 3 valores y calcular la suma de los 3 valores.
+
+Ejercicio 4
+ingresar dos valores y mostrar la suma de esos valores en un párrafo, y, en otro párrafo el resultado de la multiplicación de esos dos valores. Se recomienda capturar los 2 valores una única vez y luego operar con ellos en una única función.
+
+Ejercicio 5
+ingresar el lado de un cuadrado y calcular y mostrar su área. El área de un cuadrado es el cuadrado de la longitud del lado.
+
+Ejercicio 6
+ingresar un valor en pesos uruguayos y el valor de tipo de cambio de la fecha y calcular el equivalente en dólares.
+
+Ejercicio 7
+ingresar 2 valores y calcular y mostrar el resto de dividir el primero entre el segundo.
+
+Ejercicio 8
+ingresar 3 valores y calcular la suma de los primeros dos menos el tercero.
+
+Ejercicio 9
+ingresar 3 valores y calcular el primero elevado al cuadrado menos la suma de los otros 2.
+
+Ejercicio 10
+ingresar un importe y un porcentaje de recargo y calcular el importe final.
+
+Ejercicio 11
+ingresar un importe (subtotal), agregarle el iva (22%) y calcular y mostrar el importe final.
+
+Ejercicio 12
+solicitar ingresos de peso y altura para calcular el índice de masa corporal (IMC). Se asume que el peso se ingresó en kilogramos y la altura en metros.
+El índice de masa corporal IMC es una medida de asociación entre la masa y la talla de un individuo ideada por el estadístico belga Adolphe Quetelet.
+Calcula según la expresión matemática IMC igual masa sobre estatura al cuadrado donde la masa se expresa en kilogramos y el cuadrado de la estatura en metros cuadrados.
+IMC= masa/estatura²
+
+Ejercicio 13
+crear un botón y un párrafo. Se solicita llevar la cuenta de la cantidad de veces que el botón fue presionado en el párrafo. La cuenta comenzará en cero y se incrementará en uno con cada presión del botón. No se pide generar ningún campo de texto de ingreso de datos.
+
+Ejercicio 14
+crear un botón y un párrafo de texto al presionar el botón incrementar un contador de 3 en 3. Mostrar el valor actual del contador en el párrafo cada vez que se presione el botón.
+
+Ejercicio 15
+crear un campo de texto y un botón para que cada vez que se ingrese un número en el campo de texto se acumule el total en una variable punto al presionar otro botón mostrar el resultado final acumulado.
+
+Ejercicio 16
+para un equipo de fútbol el cálculo de los puntos obtenidos en el campeonato se asigna de la siguiente manera:
+• 3 puntos por cada partido ganado.
+• Un punto por cada partido empatado.
+• Ningún punto por partido perdido.
+Solicitar el ingreso de partidos ganados, empatados y perdidos, y calcular el total de puntos obtenidos por este equipo.
+Tener en cuenta que si bien un dato se va a solicitar no es relevante para los cálculos.
+
+Ejercicio 17
+utilizando la ley de dolbear ingresar los chirridos por minuto de un grillo y calcular la temperatura actual. El resultado obtenido estará en Fahrenheit por lo que una vez obtenido el resultado se deberá mostrar, pero además convertir a grados Celsius para también mostrarlo.
+También se puede hacer el cálculo directamente en Celsius.
+N 60 es el número de chirridos por minuto. Se sugiere hacer el cálculo en Fahrenheit y convertirlo a celsius para luego hacer el cálculo directamente hoy en celsius y ver si coinciden. Cuál tener en cuenta que la coincidencia no será exacta, pero si debe ser muy cercana.
--- codigo.js
+++ codigo.js
@@ -23,6 +23,39 @@ function misEventos() {
   document
     .querySelector("#btnCalcularEquivalenteEnDolar")
     .addEventListener("click", calcularCambioDePesosADolares);
+
+  //7
+  document
+    .querySelector("#btnDividir")
+    .addEventListener("click", calcularRestoDeDivision);
+  //8
+  document
+    .querySelector("#btnCalcularOperacionEj8")
+    .addEventListener("click", calcularSumaDosNumerosMenosTercerNumero);
+  //9
+  document
+    .querySelector("#btnCalcularOperacionEj9")
+    .addEventListener("click", calculoEntreTresValoresEj9);
+  //10
+  document
+    .querySelector("#btnImportefinal10")
+    .addEventListener("click", calcularImporteFinal);
+  //11
+  document
+    .querySelector("#btnImportefinal11")
+    .addEventListener("click", calcularImporteFinalConIva);
+  //12
+  document
+    .querySelector("#btnIndicemasa12")
+    .addEventListener("click", calcularIndiceMasaCorporal);
+  //13
+  document
+    .querySelector("#btncantidad13")
+    .addEventListener("click", contarCantidadDeVecesQuePresionanBoton);
+  //14
+  document
+    .querySelector("#btncantidad14")
+    .addEventListener("click", incrementarContadorDeVecesQuePresionanBoton);
 }
 
 //ejercicio 1
@@ -79,3 +112,94 @@ function calcularAreaDeCuadrado() {
 }
 
 // ejercicio 6
+function calcularCambioDePesosADolares() {
+  let Pesos = document.querySelector("#txtPesosej6").value;
+  let PesosNum = Number(Pesos);
+  let dolar = document.querySelector("#txtTipoDeCambioej6").value;
+  let dolarNum = Number(dolar);
+  let Cambio = PesosNum / dolarNum;
+  document.querySelector("#txtMostrarCambioDolaresj6").innerHTML =
+    Cambio + " USD ";
+}
+//ejercicio 7
+function calcularRestoDeDivision() {
+  let Dividendo = document.querySelector("#txtDividendoej7").value;
+  let DivNum = Number(Dividendo);
+  let Divisor = document.querySelector("#txtDivisorej7").value;
+  let DivsorNum = Number(Divisor);
+  let Resto = DivNum % DivsorNum;
+  document.querySelector("#txtRestoej7").innerHTML = "Resto es " + Resto;
+}
+
+//ejercicio 8
+function calcularSumaDosNumerosMenosTercerNumero() {
+  let valorsum1 = document.querySelector("#txtValor1ej8").value;
+  let valorsum2 = document.querySelector("#txtValor2ej8").value;
+  let valorrest3 = document.querySelector("#txtValor3ej8").value;
+  let NumValorsum1 = Number(valorsum1);
+  let NumValorsum2 = Number(valorsum2);
+  let NumValorrest3 = Number(valorrest3);
+  let Calculoeje8 = NumValorsum1 + NumValorsum2 - NumValorrest3;
+  document.querySelector("#txtCalculodeje8").innerHTML =
+    "la suma de Numero 1 + Numero 2 Menos el Numero 3 es " + Calculoeje8;
+}
+//ejercicio 9
+function calculoEntreTresValoresEj9() {
+  let Valor1eje9 = document.querySelector("#txtValor1eje9").value;
+  let Valor2eje9 = document.querySelector("#txtValor2eje9").value;
+  let Valor3eje9 = document.querySelector("#txtValor3eje9").value;
+  let NumValor1eje9 = Number(Valor1eje9);
+  let NumValor2eje9 = Number(Valor2eje9);
+  let NumValor3eje9 = Number(Valor3eje9);
+  let alcuadrado = NumValor1eje9 * NumValor1eje9;
+  let suma2y3eje9 = NumValor2eje9 + NumValor3eje9;
+  let Resultadoej9 = alcuadrado - suma2y3eje9;
+
+  document.querySelector("#txtResultadoej9").innerHTML =
+    "El numro 1 elevado al cuadrado menos la suma de numero 1 y 2  es =  " +
+    Resultadoej9;
+}
+//ejercicio 10
+function calcularImporteFinal() {
+  let importe10 = document.querySelector("#txtImporte10").value;
+  let recargo10 = document.querySelector("#txtRecargo10").value;
+  let NumImporte10 = Number(importe10);
+  let NumRecargo10 = Number(recargo10);
+  let CalculoPorcentaje10 = (NumImporte10 * NumRecargo10) / 100;
+  let importefinal10 = NumImporte10 + CalculoPorcentaje10;
+  document.querySelector("#txtImporteFinal10").innerHTML = importefinal10;
+}
+//ejercicio 11
+function calcularImporteFinalConIva() {
+  let Subtotal11 = document.querySelector("#txtSubtotal11").value;
+  let NumSubtotal11 = Number(Subtotal11);
+  let CalculoPorcentaje11 = (NumSubtotal11 * 22) / 100;
+  let importefinal11 = NumSubtotal11 + CalculoPorcentaje11;
+  document.querySelector("#txtImporteFinal11").innerHTML = importefinal11;
+}
+//ejercicio 12
+
+function calcularIndiceMasaCorporal() {
+  let pesokg = document.querySelector("#txtPeso12").value;
+  let alturaMts = document.querySelector("#txtAltura12").value;
+  let Numpesokg = Number(pesokg);
+  let NumalturaMts = Number(alturaMts);
+  let estatura12 = NumalturaMts * NumalturaMts;
+  let Imc = Numpesokg / estatura12;
+  document.querySelector("#txtMostrarImc12").innerHTML = Imc;
+}
+
+//ejercicio 13
+let contador13 = 0;
+function contarCantidadDeVecesQuePresionanBoton() {
+  contador13 = contador13 + 1;
+  document.querySelector("#txtCuentaVeces13").innerHTML =
+    contador13 + " clicks";
+}
+//ejercicio 14
+let contador14 = 0;
+function incrementarContadorDeVecesQuePresionanBoton() {
+  contador14 = contador14 + 3;
+  document.querySelector("#txtCuentaVeces14").innerHTML =
+    contador14 + " clicks";
+}
